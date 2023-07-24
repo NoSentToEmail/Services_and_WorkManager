c7e3d8ffb81b65d77c227da75e39215d9b3b7b1e
NoSentToEmail <saniamax20001@gmail.com>
2023-07-24 17:22:32 +0300

    Initial commit

---
+++ app/src/main/java/com/example/services_and_workmanager/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.services_and_workmanager
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+import com.example.services_and_workmanager.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        Log.d("MyService", "Start")
+        binding.simpleService.setOnClickListener{
+            Log.d("MyService", "Start")
+            startService(MyService.newIntent(this, 25))
+        }
+        binding.foregroundService.setOnClickListener{
+            ContextCompat.startForegroundService(
+                this,
+                MyforegroundService.newIntent(this)
+            )
+        }
+    }
+
+//    private fun showNotification(){
+//        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            val notificatioChannel =  NotificationChannel(
+//                CHANNEL_ID,
+//                CHANNEL_NAME,
+//                NotificationManager.IMPORTANCE_DEFAULT
+//            )
+//            notificationManager.createNotificationChannel(notificatioChannel)
+//        }
+//
+//        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+//            .setContentTitle("Title")
+//            .setContentText("Text")
+//            .setSmallIcon(R.drawable.ic_launcher_background)
+//            .build()
+//
+//        notificationManager.notify(1, notification)
+//    }
+//
+//    companion object{
+//        private const val CHANNEL_ID = "channel_id"
+//        private const val CHANNEL_NAME = "channel_name"
+//
+//    }
+}
---
+++ app/src/main/java/com/example/services_and_workmanager/MyforegroundService.kt
@@ -0,0 +1,93 @@
+package com.example.services_and_workmanager
+
+import android.annotation.SuppressLint
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.Service
+import android.content.Context
+import android.content.Context.NOTIFICATION_SERVICE
+import android.content.Intent
+import android.os.Build
+import android.os.IBinder
+import android.os.Message
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat.getSystemService
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
+
+class MyforegroundService : Service() {
+
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+
+    @SuppressLint("NotificationId0")
+    override fun onCreate() {
+        super.onCreate()
+        log("onCreate")
+        notificationManager()
+        startForeground(NOTIFICATION_ID, notification())
+    }
+
+    private fun notification() = NotificationCompat.Builder(this, CHANNEL_ID_2)
+            .setContentTitle("Title")
+            .setContentText("Text")
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .build()
+
+
+    private fun notificationManager() {
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificatioChannel = NotificationChannel(
+                CHANNEL_ID_2,
+                CHANNEL_NAME_2,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(notificatioChannel)
+        }
+    }
+
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        log("onStartCommand")
+        coroutineScope.launch {
+            for (i in 0 until 100) {
+                delay(1000)
+                log("Timer $i")
+            }
+        }
+        return START_STICKY
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        coroutineScope.cancel()
+        log("onDestroy")
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+
+    private fun log(message: String) {
+        Log.d("MyService", "MyforegroundService: $message")
+    }
+
+    companion object {
+        private const val EXTRA_START = "start"
+        private const val CHANNEL_ID_2 = "channel_id"
+        private const val CHANNEL_NAME_2 = "channel_name"
+        private const val NOTIFICATION_ID = 1
+
+        fun newIntent(context: Context): Intent {
+            return Intent(context, MyforegroundService::class.java)
+        }
+    }
+
+
+}
